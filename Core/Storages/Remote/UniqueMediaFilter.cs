91fc7dee1d29c038e4d7595901f41514176240e5
AndreyMI <im.andrey@gmail.com>
2022-08-10 17:07:23 +0300

    Periodical cache trimming

--- Core/Storages/Remote/UniqueMediaFilter.cs
+++ Core/Storages/Remote/UniqueMediaFilter.cs
@@ -3,14 +3,16 @@ using MimeTypes;
 using Serilog;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography;
 
 namespace Core.Storages.Remote
 {
     public class UniqueMediaFilter
     {
-        //TODO trim cache periodically
         private readonly Dictionary<string, string> storage = ServerConfig.Instance.MediaWarmup;
+        private readonly object locker = new object();
+        private const int TrimTreshold = 1000000;
 
         public bool NeedStore(ImageUrl image)
         {
@@ -22,16 +24,31 @@ namespace Core.Storages.Remote
             using (var sha = SHA256.Create())
             {
                 var hash = Convert.ToBase64String(sha.ComputeHash(image.Data));
-                var found = storage.TryGetValue(hash, out var path);
-
-                if (!found)
+                lock (locker)
                 {
-                    path = DateTimeOffset.UtcNow.ToString(@"\/yyyy\/MM\/dd\/") + ObjectId.New() + MimeTypeMap.GetExtension(image.MimeType);
-                    storage[hash] = path;
-                }
+                    var found = storage.TryGetValue(hash, out var path);
+                    if (!found)
+                    {
+                        path = DateTimeOffset.UtcNow.ToString(@"\/yyyy\/MM\/dd\/") + ObjectId.New() + MimeTypeMap.GetExtension(image.MimeType);
+                        storage[hash] = path;
+                        Trim(storage);
+                    }
 
-                image.Stored = path;
-                return !found;
+                    image.Stored = path;
+                    return !found;
+                }
+            }
+        }
+
+        private static void Trim(Dictionary<string, string> storage)
+        {
+            if (storage.Count > TrimTreshold)
+            {
+                var keys = storage.OrderBy(x => x.Value).Take(TrimTreshold / 2).Select(x => x.Key).ToList();
+                foreach (var key in keys)
+                {
+                    storage.Remove(key);
+                }
             }
         }
     }
