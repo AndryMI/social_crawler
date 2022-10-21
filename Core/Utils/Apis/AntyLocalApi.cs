7fe5333319ea838dd75ae6148fa8dbee422cc64c
AndreyMI <im.andrey@gmail.com>
2022-10-21 16:15:19 +0300

    Dolphin Anty Local API client

---
+++ Core/Utils/Apis/AntyLocalApi.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using Serilog;
+using System;
+
+namespace Core
+{
+    public class AntyLocalApi : ApiClient
+    {
+        public AntyLocalApi(string host = "http://localhost:3001/v1.0") : base(host)
+        {
+        }
+
+        public Automation Start(int browserProfileId)
+        {
+            try
+            {
+                var info = JsonConvert.DeserializeObject<Info>(Request("GET", $"/browser_profiles/{browserProfileId}/start?automation=1"));
+                return info.automation;
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Failed to start Anty {ProfileId}", browserProfileId);
+                return null;
+            }
+        }
+
+        public bool Stop(int browserProfileId)
+        {
+            var info = JsonConvert.DeserializeObject<Info>(Request("GET", $"/browser_profiles/{browserProfileId}/stop"));
+            return info.success;
+        }
+
+        public class Automation
+        {
+            [JsonProperty]
+            public readonly int port;
+            [JsonProperty("wsEndpoint")]
+            public readonly string endpoint;
+        }
+
+        private class Info
+        {
+            public bool success = false;
+            public Automation automation = null;
+        }
+    }
+}
